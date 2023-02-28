68006543bb54b1a3f961276604ee828678e7f597
María José Burguete Euán <marijo.mjbe@gmail.com>
2023-02-28 16:51:30 -0600

    Login Init

---
+++ src/Components/Login/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+
+  return (
+    <div className="Container">
+      <div className="Left-container">
+
+      </div>
+      <div className="Right-container">
+
+      </div>
+    </div>
+  );
+}
+
+export default Login;
