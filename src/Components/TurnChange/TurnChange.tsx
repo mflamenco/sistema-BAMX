227c865e2ef711906eceb11fb0a0b61249a592f2
mflamenco <56646615+mflamenco@users.noreply.github.com>
2023-03-02 15:44:53 -0600

    reshape style for accurate resolution

--- src/Components/TurnChange/TurnChange.tsx
+++ src/Components/TurnChange/TurnChange.tsx
@@ -1,17 +1,64 @@
-import React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
 import './TurnChange.css';
+import { ReactComponent as LogoutIcon} from '../../Assets/Logo_bamx.svg';
+
+const TurnButton = styled(Button)({
+  padding: '1vh 0',
+  color: '#F7A600',
+  borderRadius: '20px',
+  border: '6px solid #F7A600;',
+  width: '75%',
+  fontWeight: '700',
+  textTransform: 'capitalize',
+  fontSize: '5vmin',
+  fontFamily: [
+    'DM Sans',
+    'sans-serif',
+  ].join(','),
+  '&:hover': {
+    backgroundColor: '#F7A600',
+    color: 'white'
+  },
+});
+
+const LogoutButton = styled(Button)({
+  color: 'white',
+  display: 'flex',
+  fontSize: '4vmin',
+  justifySelf: 'flex-end',
+  fontFamily: [
+    'Bebas Neue',
+    'cursive',
+  ].join(','),
+  '&:hover': {
+    backgroundColor: 'white',
+    opacity: '70%',
+    color: '#E96D10',
+    
+  },
+});
+
 
 function TurnChange() {
 
   return (
     <div className="Turn-change">
+      <LogoutButton startIcon={<LogoutIcon />}>Cerrar Sesión</LogoutButton>
       <div className="Container">
-        <text className='Label'> En Atención en esta caja</text>
-        <div className="Turn">
-          <text> turno 1</text>
-          <text> comunidad A</text>
-
+        <div className="Container-top">
+          <text className='h1'> Actualmente asistiendo a</text>
+          <div className="Turn">
+            <div className="Turn-label">
+              <text> turno 1</text>
+              <text> comunidad A</text>
+            </div>
+            <TurnButton>
+              Finalizar turno
+            </TurnButton>
+          </div>
         </div>
+        <text className='h2'> *  Recuerda presionar el botón una vez que termines de atender a la comunidad</text>
       </div>
     </div>
   );
