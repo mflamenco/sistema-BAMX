1c32fdeadf590f9dd3d7932538281aa78bc0fb00
María José Burguete Euán <marijo.mjbe@gmail.com>
2023-03-03 22:35:03 -0600

    First draft

---
+++ src/Components/WindowSelection/WindowSelection.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './WindowSelection.css';
+
+function WindowSelection() {
+
+  return (
+    <div className="Root-Container">
+      <div className="Container">
+
+      </div>
+    </div>
+  );
+}
+
+export default WindowSelection;
