0fe5a5fd1e740da49e3a4580afd92ef5541c0b38
María José Burguete Euán <marijo.mjbe@gmail.com>
2023-03-09 13:29:33 -0600

    Fix logo position

--- src/Components/TurnChange/TurnChange.tsx
+++ src/Components/TurnChange/TurnChange.tsx
@@ -1,9 +1,11 @@
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
-import './TurnChange.css';
 import { useCallback, useState } from "react";
-import { ReactComponent as LogoutIcon} from '../../Assets/Icon_logout.svg';
-import { ReactComponent as LinkIcon} from '../../Assets/Icon_link.svg';
+import { ReactComponent as LinkIcon } from '../../Assets/Icon_link.svg';
+import { ReactComponent as LogoutIcon } from '../../Assets/Icon_logout.svg';
+import logo from '../../Assets/Logo_bamx.svg';
+import './TurnChange.css';
+
 
 
 const TurnButton = styled(Button)({
@@ -66,34 +68,37 @@ function TurnChange() {
 
   return (
     <div className="Turn-change">
-      <div className="Button-container">
-        <BarButton 
-        onMouseEnter={() => hoverHandler(true,1)}
-        onMouseLeave={() => hoverHandler(false,1)} 
-        startIcon={<LinkIcon className={hoverLink} width={'2vw'} />} >
-          Actualizar google sheets
-        </BarButton>
-        <BarButton 
-        onMouseEnter={() => hoverHandler(true,0)}
-        onMouseLeave={() => hoverHandler(false,0)} 
-        startIcon={<LogoutIcon className={hoverLogout} width={'2vw'} />} >
-          Cerrar Sesión
-        </BarButton>
-      </div>
-      <div className="Container">
-        <div className="Container-top">
-          <text className='h1'> Actualmente asistiendo a</text>
-          <div className="Turn">
-            <div className="Turn-label">
-              <text> turno 1</text>
-              <text> comunidad A</text>
-            </div>
-            <TurnButton>
-              Finalizar turno
-            </TurnButton>
-          </div>
+      <img src={logo}/>
+      <div className='Container'>
+        <div className="Button-container">
+          <BarButton 
+          onMouseEnter={() => hoverHandler(true,1)}
+          onMouseLeave={() => hoverHandler(false,1)} 
+          startIcon={<LinkIcon className={hoverLink} width={'2vw'} />} >
+            Actualizar google sheets
+          </BarButton>
+          <BarButton 
+          onMouseEnter={() => hoverHandler(true,0)}
+          onMouseLeave={() => hoverHandler(false,0)} 
+          startIcon={<LogoutIcon className={hoverLogout} width={'2vw'} />} >
+            Cerrar Sesión
+          </BarButton>
         </div>
-        <text className='h2'> *  Recuerda presionar el botón una vez que termines de atender a la comunidad</text>
+        <div className="Container-body">
+          <div className="Container-top">
+            <text className='h1'> Actualmente asistiendo a</text>
+            <div className="Turn">
+              <div className="Turn-label">
+                <text> turno 1</text>
+                <text> comunidad A</text>
+              </div>
+              <TurnButton>
+                Finalizar turno
+              </TurnButton>
+            </div>
+          </div>
+          <text className='h2'> *  Recuerda presionar el botón una vez que termines de atender a la comunidad</text>
+        </div>
       </div>
     </div>
   );
